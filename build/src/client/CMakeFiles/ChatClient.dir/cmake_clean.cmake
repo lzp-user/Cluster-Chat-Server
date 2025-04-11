48baec5b7e40f8044e29a1e06e44e9abdc4502b3
LZP <englishname-lzp@qq.com>
2025-04-11 13:57:09 +0800

    修改autobuild

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/rootlzp/Cluster-Chat-Server/bin/ChatClient"
+  "/home/rootlzp/Cluster-Chat-Server/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
